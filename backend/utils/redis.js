8ce6498c8cd28c61986ec8924cf08bbc9fec6121
Naimish Omar <naimishomar@gmail.com>
2026-01-18 19:12:50 +0530

    deployment

--- backend/utils/redis.js
+++ backend/utils/redis.js
@@ -1,23 +1,8 @@
 import { createClient } from 'redis';
-const redisURL = process.env.REDIS_URL || 'redis://localhost:6379';
 
-const client = createClient({
-    url: redisURL,
-    socket: {
-        tls: redisURL.startsWith('rediss://'),
-        rejectUnauthorized: false 
-    }
-});
-
-client.on('connect', () => console.log('Redis Client Connected ✅'));
-client.on('error', (err) => console.log('Redis Client Error ❌', err));
-
-const connectRedis = async () => {
-    if (!client.isOpen) {
-        await client.connect();
-    }
-};
-
-connectRedis();
+const client = createClient();
+client.on('connect', () => console.log('Redis Client Connected✅'));
+client.on('error', (err) => console.log('Redis Client Error', err));
+await client.connect();
 
 export default client;
--- client/context/axiosConfig.ts
+++ client/context/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const API_URL = "http://10.21.70.187:3000";
+const API_URL = "https://fync-backend-ptoy.onrender.com";
 
 axios.defaults.baseURL = API_URL;
 
