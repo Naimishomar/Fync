316a6b7ad8e0f6d7ad4736133ddf3adc57a446d6
Anand-Kumar-63 <anandks8013@gmail.com>
2026-03-24 11:22:15 +0530

    Hackathon{Team model}

--- backend/models/hackathon/team.model.js
+++ backend/models/hackathon/team.model.js
@@ -1,6 +1,41 @@
-import {v4 as uuidv4} from "uuid";
 import mongoose from "mongoose";
-
-const hackthonteams = new mongoose.Schema({
-        
-})
+const teamSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    Hackathon:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Hackathon",
+        required:true,
+    },
+    leader:{
+       type:mongoose.Schema.Types.ObjectId,
+       ref:"User",
+       required:true
+    },
+    members:[
+        {
+            user:{type:mongoose.Schema.Types.ObjectId},
+            role:{type:String,enum:["leader","member"],default:"member"},
+            joinedAt:{type:Date,default:Date.now()}
+        }
+    ],
+    invites:[
+        {
+            to:{type:mongoose.Schema.Types.ObjectId, ref:"User"},
+            status:{type:String,enum:["accepted","pending","declined"],default:"pending"},
+            sentAt:{type:Date,default:Date.now()}
+        }
+    ],
+    joinRequests:[
+        {
+            from:{type:mongoose.Schema.Types.ObjectId,ref:"User"},
+            message:{type:String,required:true},
+            status:{type:String,enum:["accepted","pending","declined"],default:"pending"},
+            sentAt:{type:Date,default:Date.now()}
+        }
+    ]
+},{timestamps:true})
+const HackathonTeam = mongoose.model("HackathonTeam",teamSchema);
+export default HackathonTeam;
